2db91c1086d85782a27c5a0d9e5489fac4cc6bd7
王胤驊 <66195744+unouno3388@users.noreply.github.com>
2025-12-02 13:10:09 +0800

    代碼更新

---
+++ View/StandardCalculator.xaml.cs
@@ -0,0 +1,19 @@
+using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+
+    public sealed partial class StandardCalculator :  Page
+    {
+        public StandardCalculator()
+        {
+
+            this.InitializeComponent();
+        }
+    }
+}
